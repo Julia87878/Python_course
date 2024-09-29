033c32d33c7cb9d03fed2057ced9a4f337495578
Julia Adonina <julie014@mail.ru>
2024-09-29 20:57:26 +0300

    funktion sort_by_date small fixes

--- src/processing.py
+++ src/processing.py
@@ -7,6 +7,6 @@ def filter_by_state(dict_list: list[dict], state_1: str = "EXECUTED") -> list[di
     return new_dict_list
 
 
-def sort_by_date(dict_list, ascending=True):
+def sort_by_date(dict_list: list[dict], ascending=True) -> list[dict]:
     """Функция, которая сортирует по дате"""
     return sorted(dict_list, key=lambda dic: dic.get("date"), reverse=ascending)
